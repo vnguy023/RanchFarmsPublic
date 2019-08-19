388ba17bdab1760dee32535e702fd230a89fbcf8
Vincent Nguyen <vincentnguyen88@hotmail.com>
2019-08-19 15:56:57 -0700

    Initial Commit

---
+++ RanchFarms/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  RanchFarms
+//
+//  Created by Vincent Nguyen on 8/19/19.
+//  Copyright © 2019 LightTech LLC. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
