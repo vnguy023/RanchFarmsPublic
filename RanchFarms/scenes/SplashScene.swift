5d4ec43ec0458ddd1cbe21ff93886c79dbb760f9
Vincent Nguyen <vincentnguyen88@hotmail.com>
2019-09-02 17:34:39 -0700

    - added inputController to splash scene to facillitate GameController input

--- RanchFarms/scenes/SplashScene.swift
+++ RanchFarms/scenes/SplashScene.swift
@@ -1,8 +1,12 @@
 import SpriteKit
 import GameplayKit
+import GameController
 
 class SplashScene: BaseScene {
+    var inputController = InputController()
+
     override func didMove(to view: SKView) {
+        assignActions()
     }
 
     func touchDown(atPoint pos : CGPoint) {
@@ -36,8 +40,58 @@ class SplashScene: BaseScene {
         }
     }
 
-
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        inputController.update()
     }
+
+    private func assignActions() {
+        inputController.clearHandles()
+
+        //inputController.handleMove = actionMove
+        inputController.handlePrimary[.ClickDown] = actionPrimary
+        //inputController.handleMenu[.ClickDown] = actionMenu
+        //inputController.handleUse[.ClickDown] = actionUse
+    }
+
+    private func actionPrimary() {
+        vc!.loadGameScene()
+    }
+
+    func processControllerInput() {
+        // TODO: Figure out how to do this properly for multiple people/controllers
+        for gc in GCController.controllers() {
+            if gc.playerIndex == .indexUnset {
+                // need to assign a player to this
+                // for now setting all to player 1
+                gc.playerIndex = .index1
+            }
+
+            var player = PlayerIndex.PlayerOne
+            switch gc.playerIndex {
+            case .index1:
+                player = .PlayerOne
+            default: break
+            }
+
+            let moveVector = CGVector(dx: CGFloat(gc.extendedGamepad!.leftThumbstick.xAxis.value),
+                                      dy: CGFloat(gc.extendedGamepad!.leftThumbstick.yAxis.value))
+            let movePower = min(moveVector.getMagnitude(), CGFloat(1))
+            inputController.SetLeftThumbStick(player: player, direction: moveVector, power: movePower)
+
+            inputController.pressedDown(inputKey: .Primary, value: gc.extendedGamepad!.buttonA.isPressed)
+            inputController.pressedDown(inputKey: .Cancel, value: gc.extendedGamepad!.buttonB.isPressed)
+            inputController.pressedDown(inputKey: .Use, value: gc.extendedGamepad!.buttonX.isPressed)
+            inputController.pressedDown(inputKey: .Menu, value: gc.extendedGamepad!.buttonY.isPressed)
+
+            inputController.pressedDown(inputKey: .SwitchLeft, value: gc.extendedGamepad!.leftShoulder.isPressed)
+            inputController.pressedDown(inputKey: .SwitchRight, value: gc.extendedGamepad!.rightShoulder.isPressed)
+
+            inputController.pressedDown(inputKey: .DPadUp, value: gc.extendedGamepad!.dpad.up.isPressed)
+            inputController.pressedDown(inputKey: .DPadDown, value: gc.extendedGamepad!.dpad.down.isPressed)
+            inputController.pressedDown(inputKey: .DPadLeft, value: gc.extendedGamepad!.dpad.left.isPressed)
+            inputController.pressedDown(inputKey: .DPadRight, value: gc.extendedGamepad!.dpad.right.isPressed)
+        }
+    }
+
 }
